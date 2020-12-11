1221dd486f898a79fd1bddbfe5aa410c8c28ad75
Wick McLean <17318601+Wick-McLean@users.noreply.github.com>
2020-12-11 18:21:54 -0500

    Update SaveToS3.py

--- SaveToS3.py
+++ SaveToS3.py
@@ -9,7 +9,7 @@ def save_to_s3(local_file, bucket, s3_file, now):
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
                       aws_secret_access_key=SECRET_KEY)
     print(s3)
-    response = s3.list_objects_v2(Bucket='nymag-scrape-articles-from-python')
+    response = s3.list_objects_v2(Bucket='AWS_BUCKET_HERE')
     print(response)
     try:
         s3.upload_file(local_file, bucket, s3_file + " " + now)
@@ -23,7 +23,3 @@ def save_to_s3(local_file, bucket, s3_file, now):
         return False
 
 
-# uploaded = upload_to_aws('C:\\Users\\Wick\\.PyCharmCE2019.3\\config\\scratches\\scratch_2.py',
-#                          'nymag-scrape-articles-from-python', 'teste')
-
-# botofiles = save_to_s3('speech_list.txt', 'nymag-scrape-articles-from-python', 'Whitehouse Speech')
